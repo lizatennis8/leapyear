d24103d66f46c46610f79a8a9ef2ff5b022963d4
Liza Tennis and Victoria Martinez <liza@>
2018-08-15 08:34:40 -0700

    complete first spec of false

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// User/Front End Logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
+
+
+// Business Logic
+var leapYear = function(year) {
+  return false;
+};
