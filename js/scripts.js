4ad99aa9fcfc08b4b1976cce4787eb014d75cddf
Liza Tennis and Victoria Martinez <liza@>
2018-08-15 10:54:22 -0700

    add user interface logic to reveal text for result

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// Business Logic
+var leapYear = function(year) {
+if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+  return true;
+  } else {
+    return false;
+  }
+};
+
+
 // User/Front End Logic
 
 $(document).ready(function() {
@@ -5,16 +15,15 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+        $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
-
-
-// Business Logic
-var leapYear = function(year) {
-if ((year % 4 === 0) && (year % 100 !== 0)) {
-  return true;
-  } else {
-    return false;
-  }
-};
